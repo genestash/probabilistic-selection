9d0b63877dfad64549b4eedf78b33c11a0bd707f
gene <eugenefedchenco@gmail.com>
2025-07-03 04:23:12 +0300

    feature: add export default

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
 import { adjustDistribution } from './distributionManagement';
 import { selectKey, addKey, removeKey } from './keysManagement';
+
+export default {
+    selectKey,
+    addKey,
+    removeKey,
+    adjustDistribution
+};
+
 export { selectKey, addKey, removeKey, adjustDistribution };
